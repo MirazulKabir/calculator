17c24110a66d87dce2f9ca1318785508f9a72166
MirazBhuiyan <mirazulkabirbhuiyan@gmail.com>
2020-12-21 14:16:31 +0600

    UI setup for basic calculator

---
+++ CalculatorWithUnity/Assets/Scripts/CalcButtonClickHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Calculator
+{
+    public class CalcButtonClickHandler : MonoBehaviour
+    {
+        public void ButtonOnClick()
+        {
+            Debug.Log("Button Clicked " + gameObject.name);
+        }
+    }
+}
